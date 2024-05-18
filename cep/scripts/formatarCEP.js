92697f814bc25dd6a068b32a8a816380658c0412
aulasUnisenai <164383276+aulasUnisenai@users.noreply.github.com>
2024-05-18 11:45:18 -0300

    Update formatarCEP.js

--- cep/scripts/formatarCEP.js
+++ cep/scripts/formatarCEP.js
@@ -1,4 +1,4 @@
-export function formatarCEP(inputCEP) {
+export const formatarCEP = (inputCEP) => {
   let cep = inputCEP.value.trim().replace(/\D/g, ''); // Remove caracteres não numéricos
   cep = cep.replace(/^(\d{5})(\d{3})$/, '$1-$2'); // Insere o hífen no formato correto
 
